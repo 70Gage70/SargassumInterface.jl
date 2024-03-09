4565d65a2580647d93f48e6326e6efadf2d51277
Gage <gage.bonner@gmail.com>
2024-03-08 21:58:04 -0600

    ups

--- src/SargassumInterface.jl
+++ src/SargassumInterface.jl
@@ -3,11 +3,16 @@ module SargassumInterface
 using Pluto
 
 function run()
-    Pluto.run(notebook = joinpath(@__DIR__, "..", "interface", "BOMBinterface.jl"))
+    Pluto.run(notebook = joinpath(@__DIR__, "..", "interface", "BOMBinterface-editable.jl"))
     return nothing 
 end
 
 function __init__()
+    rm(joinpath(@__DIR__, "..", "interface", "BOMBinterface-editable.jl"), force = true)
+
+    cp(
+        joinpath(@__DIR__, "..", "interface", "BOMBinterface.jl"), 
+        joinpath(@__DIR__, "..", "interface", "BOMBinterface-editable.jl"))
     @info "Use `SargassumInterface.run()` to start the interface."
 end
 
