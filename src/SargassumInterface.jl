370e0391061ce9efde7dc7949292a6a72782751e
Gage <gage.bonner@gmail.com>
2024-03-09 20:30:54 -0600

    chmod test

--- src/SargassumInterface.jl
+++ src/SargassumInterface.jl
@@ -2,21 +2,20 @@ module SargassumInterface
 
 using Pluto, Scratch
 
-interface_cache = ""
+# interface_cache = ""
 
 function run()
-    bomb_edit = joinpath(interface_cache, "BOMBinterface-editable.jl")
-
-    Pluto.run(notebook = bomb_edit)
+    chmod(@__DIR__, 0o755)
+    Pluto.run(notebook = joinpath(@__DIR__, "BOMBinterface.jl"))
     return nothing 
 end
 
 function __init__()
-    global interface_cache = @get_scratch!("interface_cache")
-    bomb_int = joinpath(@__DIR__, "BOMBinterface.jl")
-    bomb_edit = joinpath(interface_cache, "BOMBinterface-editable.jl")
-    rm(bomb_edit, force = true)
-    cp(bomb_int, bomb_edit)
+    # global interface_cache = @get_scratch!("interface_cache")
+    # bomb_int = joinpath(@__DIR__, "BOMBinterface.jl")
+    # bomb_edit = joinpath(interface_cache, "BOMBinterface-editable.jl")
+    # rm(bomb_edit, force = true)
+    # cp(bomb_int, bomb_edit)
     @info "Use `SargassumInterface.run()` to start the interface."
 end
 
