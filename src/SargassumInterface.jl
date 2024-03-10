ddade962dd840ea3d59d758ad409a51853176bdd
Gage <gage.bonner@gmail.com>
2024-03-10 11:31:36 -0500

    ups

--- src/SargassumInterface.jl
+++ src/SargassumInterface.jl
@@ -2,32 +2,28 @@ module SargassumInterface
 
 using Pluto
 
-# interface_cache = ""
+"""
+    run(; force_retry::Bool = false)
 
-function run()
+Start the interface to `SargassumBOMB`. If `force_retry == true`, the notebook will be redownloaded.
+"""
+function run(; force_retry::Bool = false)
     f = joinpath(@__DIR__, "..", "interface", "BOMBinterface-editable.jl")
     DL_LINK = "https://raw.githubusercontent.com/70Gage70/SargassumInterface.jl/master/src/BOMBinterface.jl"
-    !isfile(f) && download(DL_LINK, f)
+    (!isfile(f) || force_retry) && download(DL_LINK, f)
     Pluto.run(notebook = f)
     return nothing 
 end
 
 function __init__()
-    # global interface_cache = @get_scratch!("interface_cache")
-    # bomb_int = joinpath(@__DIR__, "BOMBinterface.jl")
-    # bomb_edit = joinpath(interface_cache, "BOMBinterface-editable.jl")
-    # rm(bomb_edit, force = true)
-    # cp(bomb_int, bomb_edit)
     @info "Use `SargassumInterface.run()` to start the interface."
 end
 
-# import PrecompileTools
+import PrecompileTools
 
-# PrecompileTools.@compile_workload begin
-#     download(
-#         "https://raw.githubusercontent.com/70Gage70/SargassumInterface.jl/master/src/BOMBinterface.jl",
-#         joinpath(@__DIR__, "..", "interface", "BOMBinterface-editable.jl")
-#     )
-# end
+PrecompileTools.@compile_workload begin
+    f = joinpath(@__DIR__, "..", "interface", "BOMBinterface-editable.jl")
+    rm(f, force = true)
+end
 
 end # module SargassumInterface
