2275986bd034654813c3164767d8b65aa07754fa
Gage <gage.bonner@gmail.com>
2024-06-17 23:56:03 -0400

    up installer, readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+Execute the following command in the terminal to install. 
+```sh
+julia -e 'file = download("https://raw.githubusercontent.com/70Gage70/SargassumInterface.jl/b1c7b50fd2bc89fb781f05448cbb2954ffd14487/src/installer.jl"); include(file)'
+```
--- src/installer.jl
+++ src/installer.jl
@@ -1,8 +1,8 @@
 import Pkg
-Pkg.pkg"registry add"
-Pkg.pkg"registry add https://github.com/70Gage70/SargassumRegistry"
-Pkg.pkg"add SargassumInterface"
-Pkg.pkg"update"
+Pkg.Registry.add("General")
+Pkg.Registry.add(RegistrySpec(url = "https://github.com/70Gage70/SargassumRegistry"))
+Pkg.add("SargassumInterface")
+Pkg.update()
 
 using SargassumInterface
 SargassumInterface.run()
