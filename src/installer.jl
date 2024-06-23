089bd58ceb9f933c8753ef4af965a960a094fee3
Gage <gage.bonner@gmail.com>
2024-06-23 16:08:13 -0400

    fix installer

--- src/installer.jl
+++ src/installer.jl
@@ -1,6 +1,6 @@
 import Pkg
 Pkg.Registry.add("General")
-Pkg.Registry.add(RegistrySpec(url = "https://github.com/70Gage70/SargassumRegistry"))
+Pkg.Registry.add(Pkg.RegistrySpec(url = "https://github.com/70Gage70/SargassumRegistry"))
 Pkg.activate(temp = true)
 Pkg.add("SargassumInterface")
 Pkg.update()
